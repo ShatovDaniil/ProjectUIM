1ecd0fea24822a6662c5bf89119506f3f0ab2b14
Liukshin <lukshin03@gmail.com>
2024-12-07 20:32:05 +0100

    add TrainModel

--- TrainingModel.py
+++ TrainingModel.py
@@ -5,21 +5,57 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
+import pandas as pd
+import datetime as dt
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
-def train_model(preprocessedData,target_column ):
 
-    selected_features = ['min', 'fgm'] # 'tov', 'stl', 'dreb']
-    X = preprocessedData[selected_features]  # Признаки
-    print("Shape of X after selecting features:", X.shape)
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict, KFold
+from sklearn.preprocessing import StandardScaler
 
-    y = target_column.apply(lambda x: 1 if x == 'yes' else 0)
-    print("Shape of y after selecting features:", y.shape)
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.preprocessing import StandardScaler
+from DataPreprocessing import DataPreprocessing
+from joblib import dump
+import os
+from DataPreprocessing import DataPreprocessing
 
-    # Логистическая регрессия
-    #model = LogisticRegression(max_iter=20)
-    #model.fit(X, y)                                     #Я В ДУШЕ НЕ ЗНАЮ, ПОЧЕМУ ОНО ЗАСЕКАЕТСЯ НА ЭТОЙ СТРОКЕ ИЗ-ЗА ПУСТЫХ ПЕРЕМЕННЫХ
-                                    #ПОТОМУ ЧТО Я СПЕЦИАЛЬНО ВЫПРИНТЛА Х И У ПЕРЕД ТЕМ, КАК ЗАСУНУ ИХ В МОДЕЛЬ И ТАМ РАЗМЕРЫ   Shape of X after selecting features: (599, 2)
-                                    #   интернет говорит, что 1размерность у это вроде норм я хз                               Shape of y after selecting features: (599,)
+def train_model(preprocessedData):
+    X = preprocessedData.drop(columns=['target_5yrs', 'Var1', 'name'])
 
-    #return model
+    y = preprocessedData['target_5yrs']
 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    model = LogisticRegression(max_iter=1000)
+
+    #kf = KFold(n_splits=5, shuffle=True, random_state=0)
+    #cv_results = cross_val_score(model, X_train, y_train, cv=kf, scoring='accuracy')
+
+    #y = cross_val_predict(model, X_train, y_train, cv=kf)
+
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+
+
+
+    output_dir = r"C:\Users\MagicBook\PycharmProjects\pythonProject\ProjectUIM"  # Путь к вашей папке ProjectUIM
+    os.makedirs(output_dir, exist_ok=True)  # Создаем директорию, если она не существует
+    model_filename = os.path.join(output_dir, 'trained_model.joblib')  # Указываем путь для сохранения модели
+    dump(model, model_filename)  # Сохраняем модель в файл
+
+    print(f"Модель сохранена по пути: {model_filename}")
+    return model
+
+filePath = "C:\\Users\\MagicBook\\PycharmProjects\\pythonProject\\ProjectUIM"
+inputData = pd.read_csv(f"{filePath}\\TrainNBAData.csv")
+df = DataPreprocessing(inputData)
+train_model(df)
